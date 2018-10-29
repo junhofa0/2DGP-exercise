5415a00b9de6213560eef4ad6f9ddeafd3bed109
ParkChanjun(MNB) <dpldp92@daum.net>
2018-10-29 23:28:40 +0900

    ROTATE_SPEED_RPS 설정
    1초에 도는 각도 설정 PIE*4 ( 2바퀴 )

--- Drill/Drill12/boy.py
+++ Drill/Drill12/boy.py
@@ -13,6 +13,7 @@ RUN_SPEED_KMPH = 20.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+ROTATE_SPEED_RPS = 4 * 3.141592   # 1초 분의 라디안 값
 
 # Boy Action Speed
 # fill expressions correctly
