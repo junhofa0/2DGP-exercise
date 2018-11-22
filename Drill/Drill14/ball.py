565b5a1beb2ed57bf00c57138e035c2857f9230a
junhofa0 <dpldp92@daum.net>
2018-11-22 16:44:04 +0900

    ball 모듈에 main_state 모듈 추가
    ball이 그려지는 위치를 알기 위해 main_state의 background를 가지고 옮

--- Drill/Drill14/ball.py
+++ Drill/Drill14/ball.py
@@ -2,11 +2,15 @@ import random
 from pico2d import *
 import game_world
 import game_framework
+import main_state
 
 class Ball:
     image = None
-
+    bg = None
+    
     def __init__(self):
+        if Ball.bg == None:
+            Ball.bg = main_state.background
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
         self.x, self.y, self.fall_speed = random.randint(0, 1600-1), random.randint(0, 800 -1), 0
